bab84884b85d17d4324f0531ebe498899fe6667a
Marius <baumann.marius@gmail.com>
2016-05-15 14:55:56 +0200

    gg

--- DjangoWebProject/settings.py
+++ DjangoWebProject/settings.py
@@ -70,7 +70,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = path.join(PROJECT_ROOT, 'static').replace('\\', '/')
+STATIC_ROOT = 'C:/Users/mariu/Documents/Repos/jsmanagerAzure/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -89,7 +89,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    #'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
